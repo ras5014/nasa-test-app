ae9ad8176da988b72206d4a76020d9acdc5a54ff
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-12-11 23:39:04 +0530

    test: enhance launch routes tests with content type checks and error handling

--- server/src/routes/launches/launches.test.ts
+++ server/src/routes/launches/launches.test.ts
@@ -3,7 +3,64 @@ import request from "supertest";
 
 describe("Test GET /launches", () => {
   test("It should respond with 200 success", async () => {
-    const response = await request(app).get("/api/v1/launches").expect(200);
+    const response = await request(app)
+      .get("/api/v1/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
     // expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  test("It should respond with 201 created", async () => {
+    const newLaunch = {
+      mission: "Test Mission",
+      rocket: "Test Rocket",
+      target: "Kepler-442 b",
+      launchDate: "January 4, 2028",
+    };
+
+    const response = await request(app)
+      .post("/api/v1/launches")
+      .send(newLaunch)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(newLaunch.launchDate).valueOf();
+    const responseDate = new Date(response.body.data.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body.data).toMatchObject({
+      mission: "Test Mission",
+      rocket: "Test Rocket",
+      target: "Kepler-442 b",
+    });
+  });
+
+  test("It should catch missing required properties", async () => {
+    const newLaunch = {
+      mission: "Test Mission",
+      rocket: "Test Rocket",
+      target: "Kepler-442 b",
+    };
+
+    const response = await request(app)
+      .post("/api/v1/launches")
+      .send(newLaunch)
+      .expect(400);
+  });
+
+  test("It should catch invalid launch date", async () => {
+    const newLaunch = {
+      mission: "Test Mission",
+      rocket: "Test Rocket",
+      target: "Kepler-442 b",
+      launchDate: "Invalid Date",
+    };
+
+    const response = await request(app)
+      .post("/api/v1/launches")
+      .send(newLaunch)
+      .expect(400);
   });
 });
